400e92a7abc0e511d5064412452f69ad40c4703c
Tiago Barbosa <up202004926@edu.fe.up.pt>
2023-03-13 21:44:04 +0000

    texCoords for diamond correct

--- tp4/MyDiamond.js
+++ tp4/MyDiamond.js
@@ -33,10 +33,10 @@ export class MyDiamond extends CGFobject {
 
 		//define texCoords
 		this.texCoords = [
-			0, 0,
-			0, 1,
-			1, 0,
-			1, 1
+			0, 0.5,
+			0.25, 0.75,
+			0.25, 0.25,
+			0.5, 0.5
 		];
 
 		//The defined indices (and corresponding vertices)
